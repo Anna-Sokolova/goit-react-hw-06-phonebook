d262f4ae36f2faf49195302b398ceb43c26578e2
Anna Sokolova <sokolovaskorik@gmail.com>
2021-04-13 13:50:49 +0200

    Changed to createAction

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,21 +1,15 @@
+import { createAction } from '@reduxjs/toolkit';
 import types from './contacts-types';
 import shortid from 'shortid';
 
-export const addContact = data => ({
-  type: types.ADD,
-  payload: {
-    id: shortid.generate(),
-    name: data.name,
-    number: data.number,
-  },
+export const addContact = createAction(types.ADD, function prepare(data) {
+  return {
+    payload: {
+      id: shortid.generate(),
+      name: data.name,
+      number: data.number,
+    },
+  };
 });
-
-export const deleteContact = contactId => ({
-  type: types.DELETE,
-  payload: contactId,
-});
-
-export const changeFilter = value => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+export const deleteContact = createAction(types.DELETE);
+export const changeFilter = createAction(types.CHANGE_FILTER);
